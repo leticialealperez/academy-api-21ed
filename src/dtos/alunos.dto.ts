7da0ec81f9797be82da6a6ca4dfac495007d7c60
Leticia Leal <leticialealp@hotmail.com>
2025-03-17 22:47:59 -0300

    add CRUD e alunos e turmas

---
+++ src/dtos/alunos.dto.ts
@@ -0,0 +1,14 @@
+// DTO -> Data Transfer Object -> objeto de transferencia de dados estruturados
+
+export interface CadastrarAlunoDto {
+  nome: string;
+  idade?: number;
+  email: string;
+  senha: string;
+}
+
+export interface ListarAlunosDto {
+  nome?: string;
+}
+
+export type AtualizarAlunoDto = Partial<CadastrarAlunoDto> & { id: string };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import { envs } from "./envs";
 import { AlunosRoutes } from "./routes/alunos.routes";
+import { TurmasRoutes } from "./routes/turmas.routes";
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (_, res) => {
   res.status(200).json({
-    success: true,
-    message: "API is running",
+    sucesso: true,
+    mensagem: "API is running",
   });
 });
 
 app.use(AlunosRoutes.bind());
+app.use(TurmasRoutes.bind());
 
 app.listen(envs.PORT, () => console.log("Server is running"));
