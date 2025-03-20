16746688cb1a523b08de2214c98d45e18f386c50
Leticia Leal <leticialealp@hotmail.com>
2025-03-20 11:59:41 -0300

    add lógica do controller de matricula

--- src/controllers/matriculas.controller.ts
+++ src/controllers/matriculas.controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from "express";
 import { onError } from "../utils/on-error";
+import { MatriculasService } from "../services/matriculas.service";
 
 export class MatriculasController {
   public async matricular(req: Request, res: Response) {
     try {
-      //...
+      const { turmaId, alunoId } = req.params;
+
+      const service = new MatriculasService();
+      const resultado = await service.matricularAluno({ alunoId, turmaId });
+
+      res.status(201).json({
+        sucesso: true,
+        mensagem: "Aluno matriculado com sucesso",
+        dados: resultado,
+      });
     } catch (error) {
       onError(error, res);
     }
@@ -12,7 +22,16 @@ export class MatriculasController {
 
   public async listarAlunos(req: Request, res: Response) {
     try {
-      //...
+      const { turmaId } = req.params;
+
+      const service = new MatriculasService();
+      const resultado = await service.listarAlunosMatriculados(turmaId);
+
+      res.status(200).json({
+        sucesso: true,
+        mensagem: "Alunos matriculados listados com sucesso",
+        dados: resultado,
+      });
     } catch (error) {
       onError(error, res);
     }
@@ -20,7 +39,16 @@ export class MatriculasController {
 
   public async listarTurmas(req: Request, res: Response) {
     try {
-      //...
+      const { alunoId } = req.params;
+
+      const service = new MatriculasService();
+      const resultado = await service.listarTurmasDeUmAluno(alunoId);
+
+      res.status(200).json({
+        sucesso: true,
+        mensagem: "Turmas em que o aluno está matriculado listadas com sucesso",
+        dados: resultado,
+      });
     } catch (error) {
       onError(error, res);
     }
